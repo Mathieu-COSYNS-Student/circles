4e3a23259e45bef864fb208988b249b8d00cccb1
Mathieu-COSYNS-Student <mathieu.cosyns@student.uclouvain.be>
2023-04-05 03:10:20 +0200

    Update splash screen

--- circles-expo/index.ts
+++ circles-expo/index.ts
@@ -1,4 +1,8 @@
 import { registerRootComponent } from 'expo';
 import App from './App';
+import { preventAutoHideAsync } from 'expo-splash-screen';
 
+preventAutoHideAsync().catch(() => {
+  /* reloading the app might trigger some race conditions, ignore them */
+});
 registerRootComponent(App);
