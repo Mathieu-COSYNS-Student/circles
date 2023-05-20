5a73f60574bde48fdf129c3ca5c87b74cd61a8b7
Mathieu-COSYNS-Student <73463417+Mathieu-COSYNS-Student@users.noreply.github.com>
2023-05-21 00:31:18 +0200

    Add website ui

--- apps/nextjs/src/pages/_app.tsx
+++ apps/nextjs/src/pages/_app.tsx
@@ -1,11 +1,94 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ClerkProvider } from "@clerk/nextjs";
+import Image from "next/image";
+import { ClerkProvider, UserButton, useUser } from "@clerk/nextjs";
+
+import { OpenOutline } from "~/components/OpenOutline";
+
+const Header = () => {
+  const { user } = useUser();
+
+  return (
+    <header className="border-b border-gray-200 bg-white dark:border-zinc-900 dark:bg-zinc-900 dark:shadow-sm dark:shadow-zinc-700">
+      <nav className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between px-6 py-4">
+        <a href="/" className="flex items-center">
+          <Image
+            className="mr-3"
+            src="/Circles_logo_round.svg"
+            alt="circle-logo"
+            width={48}
+            height={48}
+          />
+          <span className="whitespace-nowrap text-2xl font-semibold dark:text-white">
+            Circles
+          </span>
+        </a>
+        <div className="flex md:order-2">
+          {user ? (
+            <UserButton afterSignOutUrl="/" />
+          ) : (
+            <>
+              <a
+                href="/sign-in"
+                className={`mr-2 rounded-lg border border-brand-700 px-4 py-2
+        text-center text-sm font-medium text-brand-700
+        hover:border-brand-800 hover:bg-brand-800 hover:text-white
+        focus:outline-none focus:ring-4 focus:ring-brand-300
+        dark:border-brand-600 dark:text-brand-600 dark:hover:border-brand-700 dark:hover:bg-brand-700
+        dark:focus:ring-brand-400`}
+              >
+                Sign in
+              </a>
+              <a
+                href="/sign-up"
+                className={`rounded-lg border border-brand-700 bg-brand-700 px-4 py-2
+        text-center text-sm font-medium text-white
+        hover:border-brand-800 hover:bg-brand-800 hover:text-white
+        focus:outline-none focus:ring-4 focus:ring-brand-300
+        dark:bg-brand-600 dark:hover:border-brand-700 dark:hover:bg-brand-700
+        dark:focus:ring-brand-400`}
+              >
+                Sign up
+              </a>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="mx-auto max-w-screen-xl px-6 pb-12 pt-8 text-center">
+      <p>Project made by Mathieu Cosyns and Brandon Ngoran Ntam</p>
+      <p>
+        Source code can be found on{" "}
+        <a
+          className="font-medium text-brand-600 hover:underline dark:text-brand-400"
+          href="https://github.com/Mathieu-COSYNS-Student/circles"
+          target="_blank"
+        >
+          Github{" "}
+          <span className="inline-block h-4 w-4 pt-[1px]">
+            <OpenOutline />
+          </span>
+        </a>
+      </p>
+    </footer>
+  );
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
+      <Header />
+
+      <main className="flex-grow">
+        <Component {...pageProps} />
+      </main>
+
+      <Footer />
     </ClerkProvider>
   );
 }
--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,8 +1,56 @@
+import Image from "next/image";
+
 const IndexPage = () => {
   return (
-    <div>
-      <p>This is the circles website. There is nothing here use the app!</p>
-    </div>
+    <>
+      <section className="hero py-8 before:bg-brand-600 before:dark:bg-brand-900 lg:py-16">
+        <div>
+          <h2 className="text-6xl font-semibold">
+            Stay connected with everyone around you
+          </h2>
+          <p className="my-4 text-xl">
+            Circles helps you to stay in touch with your friends, family and
+            caregivers. With Circles you can ask for help around you, manage
+            appointments and improve the work of your caregivers.
+          </p>
+          <a
+            href="#"
+            className={`my-6 inline-block rounded-lg border border-white bg-white px-4
+            py-2 text-center text-sm font-medium text-brand-700
+            hover:border-brand-800 hover:bg-brand-800 hover:text-white
+            focus:outline-none focus:ring-4 focus:ring-brand-300
+            dark:border-white dark:text-brand-600 dark:hover:border-brand-700 dark:hover:bg-brand-700
+            dark:focus:ring-brand-400`}
+          >
+            Download Circles
+          </a>
+        </div>
+        <div className="relative w-full">
+          <Image
+            className="drop-shadow-[0_16px_25px_rgba(0,0,0,0.3)] filter dark:drop-shadow-[0_16px_25px_rgba(255,255,255,0.3)]"
+            src="/mobile_template_empty.svg"
+            alt="circle-logo"
+            fill={true}
+          />
+        </div>
+      </section>
+      <svg
+        className="text-brand-600 dark:text-brand-900"
+        width="100%"
+        height="100%"
+        id="svg"
+        viewBox="0 0 1440 50"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="m1440 0v20C1302.4 9.1 1164.8 1.7 985 10 805.2 18.3 583.2 42.3 412 46c-171.2 3.7-291.6-12.9-412-29.5v-16.5z"
+          stroke="none"
+          strokeWidth="0"
+          fill="currentColor"
+          fillOpacity="1"
+        ></path>
+      </svg>
+    </>
   );
 };
 
